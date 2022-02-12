295ec9e641db0e9f6170a10f548e4a4dc7d44f15
lucasnahas <lucasahas@gmail.com>
2022-02-12 14:33:31 +0000

    Creates status component for displaying the status text

---
+++ src/Card/Status.jsx
@@ -0,0 +1,20 @@
+import { Typography, Grid } from '@mui/material';
+
+export default function Card() {
+  const styles = {
+    sx: {
+      width: '80%',
+      height: '8px',
+      borderRadius: '4px 1px 1px 1px',
+      marginTop: '-16px',
+      marginLeft: '-24px',
+    },
+  };
+  return (
+    <Grid item>
+      <Typography>
+        Status:
+      </Typography>
+    </Grid>
+  );
+}
