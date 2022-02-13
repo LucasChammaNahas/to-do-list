1bcc03cfbe2424e1bef4a7d496c12e12faf25d8a
lucasnahas <lucasahas@gmail.com>
2022-02-13 22:56:06 +0000

    Wires logic to activate timeout on change

--- src/Card/MyTextInput.jsx
+++ src/Card/MyTextInput.jsx
@@ -1,21 +1,27 @@
 import * as React from 'react';
 import { Grid } from '@mui/material';
 import TextField from '@mui/material/TextField';
+import { useGlobalState } from '../Provider';
 
 export default function MyTextInput() {
-  const [value, setValue] = React.useState('Controlled');
+  const { timeoutId } = useGlobalState();
+  const { setTimeoutBool } = useGlobalState();
+  const { setIsSaving } = useGlobalState();
 
   const handleChange = (event) => {
-    setValue(event.target.value);
+    clearTimeout(timeoutId);
+    setTimeoutBool(true);
+    setIsSaving(true);
   };
 
   return (
-      <Grid item xs={ 10 }>
+      <Grid item xs={ 12 }>
         <TextField
           placeholder="Enter Todo..."
           multiline
-          minRows={6}
+          minRows={2}
           fullWidth
+          onChange={ handleChange }
         />
       </Grid>
   );
