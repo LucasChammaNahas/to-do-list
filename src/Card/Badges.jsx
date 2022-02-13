0ac837e189b8e6a895c1c87f27d4ed01cefec186
lucasnahas <lucasahas@gmail.com>
2022-02-13 20:30:08 +0000

    Inserts global state and click handlers

--- src/Card/Badges.jsx
+++ src/Card/Badges.jsx
@@ -1,19 +1,36 @@
 import { IconButton, Grid } from '@mui/material';
 import { GppGood, GppBad, GppMaybe } from '@mui/icons-material';
 import { palette } from '../Utils/palette';
+import { useGlobalState } from '../Provider';
 
-export default function Badges() {
-  const { red, yellow, green } = palette
+export default function Badges({ index }) {
+  const { todoList, setTodoList } = useGlobalState();
+  const color = todoList[index].status;
+
+  let bad = 'gray';
+  let maybe = 'gray';
+  let good = 'gray';
+  
+  if (color === 'red') bad = palette[color];
+  if (color === 'yellow') maybe = palette[color];
+  if (color === 'green') good = palette[color];
+
+  const changeStatus = (status) => {
+    let updatedStatus = [...todoList];
+    updatedStatus[index].status = status;
+    setTodoList(updatedStatus);
+  };
+  
   return (
-    <Grid container item xs={2} direction="column" justifyContent="center" alignItems="flex-start">
-      <IconButton>
-        <GppBad fontSize="large" style={{fill: red}}  />
+    <Grid>
+      <IconButton onClick={() => changeStatus('red') }>
+        <GppBad style={{fill: bad}}  />
       </IconButton>
-      <IconButton>
-        <GppMaybe fontSize="large" style={{fill: yellow}}  />
+      <IconButton onClick={() => changeStatus('yellow') }>
+        <GppMaybe style={{fill: maybe}}  />
       </IconButton>
-      <IconButton>
-        <GppGood fontSize="large" style={{fill: green}} />
+      <IconButton onClick={() => changeStatus('green') }>
+        <GppGood style={{fill: good}} />
       </IconButton>
     </Grid>
   );
