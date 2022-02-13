6dd8dd6e38202ccce42366bc75bab9760623176c
lucasnahas <lucasahas@gmail.com>
2022-02-13 22:56:34 +0000

    Adds delete card logic

--- src/Card/CloseButton.jsx
+++ src/Card/CloseButton.jsx
@@ -1,18 +1,25 @@
 import { Grid, IconButton } from '@mui/material';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
-import { palette } from '../Utils/palette';
+import { useGlobalState } from '../Provider';
+// import { palette } from '../Utils/palette';
 
-export default function CloseButton() {
+export default function CloseButton({ index }) {
+  const { todoList, setTodoList } = useGlobalState();
   const styles = {
     sx: {
       marginRight: '-24px',
     },
   };
 
+  const deleteCard = () => {
+    const updatedList = [...todoList].filter((_,i) => i !== index );
+    setTodoList(updatedList);
+  };
+
   return (
     <Grid item>
-      <IconButton {...styles}>
-        <DeleteForeverIcon fontSize="large" style={{ fill: 'gray' }} />
+      <IconButton onClick={ deleteCard } {...styles}>
+        <DeleteForeverIcon fontSize="medium" color="primary" />
       </IconButton>
     </Grid>
   );
