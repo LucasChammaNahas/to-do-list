0ecad2964cba9681fd63a73f07bddab30967d4a1
lucasnahas <lucasahas@gmail.com>
2022-02-13 22:54:06 +0000

    Adds syncing and all saved icons logic

--- src/App/Header.jsx
+++ src/App/Header.jsx
@@ -1,30 +1,39 @@
-import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import CloudDoneIcon from '@mui/icons-material/CloudDone';
+import SyncIcon from '@mui/icons-material/Sync';
+import { createStyles, makeStyles } from "@mui/styles";
+import { useGlobalState } from '../Provider';
+
+export const useStyles = makeStyles(() =>
+  createStyles({
+    SyncIcon: { animation: "$spin 7s linear infinite" },
+    "@keyframes spin": {
+      "0%": { transform: "rotate(360deg)" },
+      "100%": { transform: "rotate(0deg)" },
+    }
+  })
+);
 
 export default function Header() {
+  const classes = useStyles();
+  const { isSaving } = useGlobalState();
+
+  const syncIcon = isSaving 
+    ? <SyncIcon className={classes.SyncIcon} style={{ fill: '#fff' }} /> 
+    : <CloudDoneIcon style={{ fill: '#fff' }} />;
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
+          <Typography>All changes saved</Typography>
+          <IconButton>
+            { syncIcon }
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
     </Box>
