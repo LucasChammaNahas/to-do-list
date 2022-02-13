70cdf2e8d0ebef864475a19dfa1acbcc8f70fbfc
lucasnahas <lucasahas@gmail.com>
2022-02-13 22:54:48 +0000

    Adds timeout logic for saving the data to the API

--- src/App/TodoList.jsx
+++ src/App/TodoList.jsx
@@ -1,16 +1,37 @@
 import { Container } from '@mui/material';
+import { useEffect } from 'react';
 import Card from '../Card'
 import { useGlobalState } from '../Provider';
 
 export default function TodoList() {
-  const { todoList, setTodoList } = useGlobalState();
-  console.log('📺🐛 ', todoList);
+  const { todoList } = useGlobalState();
+  const { setIsSaving } = useGlobalState();
+  const { timeoutId, setTimeoutId } = useGlobalState();
+  const { timeoutBool, setTimeoutBool } = useGlobalState();
 
-  const renderCards = () => todoList.map((_, index) => <Card index={ index } />);
-  
+  useEffect(() => {
+    if (timeoutBool) {
+      const id = setTimeout(() => {
+        console.log('📺🐛 ababou', id);
+        setIsSaving(false);
+      }, 1000);
+      setTimeoutId(id);
+      setTimeoutBool(false);
+    }
+  }, [setIsSaving, setTimeoutId, setTimeoutBool, timeoutBool]);
+
+  function myStopFunction() {
+    clearTimeout(timeoutId);
+    console.log('📺🐛 id stop', timeoutId);
+  }
+
+  const renderCards = () => todoList.map((_, index) => <Card index={index} />);
+
   return (
-    <Container maxWidth="sm" my={ 2 }>
-      { renderCards() }
+    <Container maxWidth="sm" my={2}>
+      <div onClick={() => myStopFunction()}>stop</div>
+      <div onClick={() => setIsSaving(true)}>set is saving true</div>
+      {renderCards()}
     </Container>
   );
 }
