38d19be12d48c8fd5331b10d7fd1b50ec1631914
lucasnahas <lucasahas@gmail.com>
2022-02-14 23:54:15 +0000

    Inserts filtering logic Inserts add todo button Updates structure

--- src/App/TodoList.jsx
+++ src/App/TodoList.jsx
@@ -1,37 +1,64 @@
-import { Container } from '@mui/material';
 import { useEffect } from 'react';
+import { Container, Box, Grid } from '@mui/material';
 import Card from '../Card'
+import AddTodoBtn from './AddTodoBtn';
 import { useGlobalState } from '../Provider';
 
 export default function TodoList() {
   const { todoList } = useGlobalState();
   const { setIsSaving } = useGlobalState();
-  const { timeoutId, setTimeoutId } = useGlobalState();
+  const { setTimeoutId } = useGlobalState();
   const { timeoutBool, setTimeoutBool } = useGlobalState();
+  const { filter, setFilter } = useGlobalState();
+
+  console.log('📺🐛 todolist', todoList);
 
   useEffect(() => {
     if (timeoutBool) {
       const id = setTimeout(() => {
-        console.log('📺🐛 ababou', id);
         setIsSaving(false);
-      }, 1000);
+      }, 3000);
       setTimeoutId(id);
       setTimeoutBool(false);
     }
   }, [setIsSaving, setTimeoutId, setTimeoutBool, timeoutBool]);
 
-  function myStopFunction() {
-    clearTimeout(timeoutId);
-    console.log('📺🐛 id stop', timeoutId);
-  }
+  const sortCards = () => {
+    let sorted;
+    if (filter.status) {
+      sorted = todoList.sort(function (a, b) {
+        let x = a.status.toLowerCase();
+        let y = b.status.toLowerCase();
+        if (x < y) { return -1; }
+        if (x > y) { return 1; }
+        return 0;
+      });
+    }
+    else if (filter.timestamp) sorted = todoList.sort(function(a, b){return a.timestamp - b.timestamp});
+    else {
+      sorted = todoList.sort(function (a, b) {
+        let x = a.msg.toLowerCase();
+        let y = b.msg.toLowerCase();
+        if (x < y) { return -1; }
+        if (x > y) { return 1; }
+        return 0;
+      });
+    }
+    return sorted;
+  };
 
-  const renderCards = () => todoList.map((_, index) => <Card index={index} />);
+  const sortedCards = sortCards();
+
+  const renderCards = () => sortedCards.map((_, index) => <Card index={index} />);
 
   return (
-    <Container maxWidth="sm" my={2}>
-      <div onClick={() => myStopFunction()}>stop</div>
-      <div onClick={() => setIsSaving(true)}>set is saving true</div>
-      {renderCards()}
+    <Container maxWidth="sm">
+      <Box my={2}>
+        <Grid container justifyContent="center">
+          <AddTodoBtn />
+        </Grid>
+        {renderCards()}
+      </Box>
     </Container>
   );
 }
