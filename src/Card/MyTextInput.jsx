afaeab6106f0fc53efd94acac4fc0264e7fe71c9
lucasnahas <lucasahas@gmail.com>
2022-02-14 23:56:06 +0000

    Updates component control

--- src/Card/MyTextInput.jsx
+++ src/Card/MyTextInput.jsx
@@ -3,25 +3,33 @@ import { Grid } from '@mui/material';
 import TextField from '@mui/material/TextField';
 import { useGlobalState } from '../Provider';
 
-export default function MyTextInput() {
+export default function MyTextInput({ index }) {
   const { timeoutId } = useGlobalState();
   const { setTimeoutBool } = useGlobalState();
   const { setIsSaving } = useGlobalState();
+  const { todoList, setTodoList } = useGlobalState();
 
-  const handleChange = (event) => {
+  const { msg } = todoList[index];
+
+  const handleChange = (value) => {
     clearTimeout(timeoutId);
     setTimeoutBool(true);
     setIsSaving(true);
+    const updatedList = [...todoList];
+    updatedList[index].msg = value;
+    setTodoList(updatedList)
+    console.log('📺🐛 value', value);
   };
 
   return (
       <Grid item xs={ 12 }>
         <TextField
+          value={ msg }
           placeholder="Enter Todo..."
           multiline
           minRows={2}
           fullWidth
-          onChange={ handleChange }
+          onChange={ (e) => handleChange(e.target.value) }
         />
       </Grid>
   );
